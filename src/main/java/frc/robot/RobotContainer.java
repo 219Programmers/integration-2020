51502fba9e7a2db27d1897abe74ca4996439fa4a
219Programmers <60157129+219Programmers@users.noreply.github.com>
2020-01-25 13:48:47 -0500

    1/25/2020

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,57 +7,23 @@
 
 package frc.robot;
 
-// imported needed libraries to use to get buttons, joysticks, and put this on smartdashbaord
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.Button;
-import edu.wpi.first.wpilibj.smartdashboard.*;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.CamMode;
+import frc.robot.commands.ClimbNow;
 import frc.robot.commands.Drive;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SwitchLED;
+import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.LimeVisionSubsystem;
 import frc.robot.Robot;
-import edu.wpi.first.wpilibj.Solenoid;
-
-// importing our code so we can program the buttons to do certain commands
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.TestMotot;
-import frc.robot.commands.*;
-
-// simeon and james code 1/23
-import frc.robot.commands.BooleanTest;
-import frc.robot.commands.CalibrateColor;
-import frc.robot.commands.CloseValve;
-import frc.robot.commands.ColorScanMove;
-import frc.robot.commands.ColorWheel;
-import frc.robot.commands.CompressCom;
-import frc.robot.commands.FindColor;
-import frc.robot.commands.ScanClosestColor;
-import frc.robot.commands.Shift;
-import frc.robot.commands.SinglePneum;
-import frc.robot.commands.SinglePneumClose;
-import frc.robot.commands.StopCompress;
-import frc.robot.commands.TurnAndStop;
-import frc.robot.commands.UsePneum;
-//import frc.robot.subsystems.ColorSensor;
-import frc.robot.subsystems.ColorSensor;
-import frc.robot.subsystems.Compression;
-import frc.robot.subsystems.Motor;
-import frc.robot.subsystems.Pneumatics;
-import frc.robot.subsystems.SinglePneumatics;
-
-
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -67,58 +33,33 @@ import frc.robot.subsystems.SinglePneumatics;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-  public static DriveTrain m_driveTrain;
-  public static TestMotot m_spin;
-  
-  public static Joystick xbox;
- // public Button a;
-  public Button b;
+  public static Joystick mainDriver;
+  public Button two;
+  public Button three;
+  public Button four;
+  public Button climb;
+  public Button descend;
   public JoystickButton lmove;
   public JoystickButton rmove;
-  public JoystickButton mistake;
-  public Drive driving;
-  // setDefaultCommand(new Drive());
-
+  public static DriveTrain m_driveTrain;
   public static LimeVisionSubsystem limeSub;
   public static SwitchLED switchLight;
-
-  //james and simeon code
-  public JoystickButton ex;
-  public JoystickButton y;
-  public JoystickButton a;
-  public static ColorSensor m_cs;
-  public static Motor m_mot;
-  public static Pneumatics pneum; // new Pneumatics(Constants.PNEUM, Constants.PNEUMCLOSE, Constants.SOLSLIDETIME);
-  public static Pneumatics lPneumShift;
-  public static Pneumatics rPneumShift;
-  public static SinglePneumatics pneumSingle; //new SinglePneumatics();
-  public static Compression comp;
-
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  public static Climb mongClimb;
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
-  public RobotContainer() {
-    m_driveTrain = new DriveTrain();
-    m_spin = new TestMotot();
-    comp = new Compression();
+  public RobotContainer()
+   {
+    // Configure the button bindings
     limeSub = new LimeVisionSubsystem();
-    m_cs = new ColorSensor();
-    m_mot = new Motor();
-    
-    lPneumShift = new Pneumatics(Constants.LEFTSHIFTOPEN, Constants.LEFTSHIFTCLOSE, Constants.SOLSLIDETIME);
-    rPneumShift = new Pneumatics(Constants.RIGHTSHIFTOPEN, Constants.RIGHTSHIFTCLOSE, Constants.RIGHTSOLSLIDETIME);
-    pneumSingle = null;
+    m_driveTrain = new DriveTrain();
     switchLight = new SwitchLED();
-    pneum = null;
-    // driving = new Drive(getXboxXSpeed(), getXboxYSpeed());
-
-    // s and j
-    SmartDashboard.putString("Calibrate", "");
-    SmartDashboard.putString("Turn Color", "");
-    SmartDashboard.putBoolean("Boolean", false);
-
+    mongClimb = new Climb();
+    mongClimb.encoderSpark1.setPosition(0);
+    SmartDashboard.putNumber("turn", 15);
+    SmartDashboard.putNumber("mongSpeed", .3);
     configureButtonBindings();
   }
 
@@ -128,77 +69,61 @@ public class RobotContainer {
    * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
+  private void configureButtonBindings() 
+  {
+    mainDriver = new Joystick(0);
+    // three = new JoystickButton(mainDriver, 1);
+    // two = new JoystickButton(mainDriver, 2);
+    lmove = new JoystickButton(mainDriver, 1);
+    rmove = new JoystickButton(mainDriver, 5);
+    four = new JoystickButton(mainDriver, 4);
+    climb = new JoystickButton(mainDriver, 1);
+    descend= new JoystickButton(mainDriver, 2);
 
-    // a.whenPressed(new Spin());
-    xbox = new Joystick(1);
-    rmove = new JoystickButton(xbox, 1);
-    lmove = new JoystickButton(xbox, 5);
-    mistake = new JoystickButton(xbox, 2);
-    ex = new JoystickButton(xbox, Constants.X_BUTTON_ID);
-    y = new JoystickButton(xbox, Constants.Y_BUTTON_ID);
-    a = new JoystickButton(xbox, Constants.A_BUTTON_ID);
-
-    JoystickButton three = new JoystickButton(xbox, 1);
-    JoystickButton two = new JoystickButton(xbox, 2);
-    lmove = new JoystickButton(xbox, 1);
-    rmove = new JoystickButton(xbox, 5);
-    JoystickButton four = new JoystickButton(xbox, 4);
-
-    three.toggleWhenPressed(new SwitchLED());
-    two.toggleWhenPressed(new CamMode());
-    
+    // three.toggleWhenPressed(new SwitchLED());
+    // two.toggleWhenPressed(new CamMode());
+    climb.toggleWhenPressed(new ClimbNow(true,Constants.CLIMBSPEED));
+    descend.toggleWhenPressed(new ClimbNow(false,Constants.CLIMBSPEED));
+    // climb.whileHeld(new ClimbNow(true,Constants.CLIMBSPEED));
+    // descend.whileHeld(new ClimbNow(false,Constants.CLIMBSPEED));
     m_driveTrain.setDefaultCommand(new Drive());
 
-
-    // s and j
-    ex.toggleWhenPressed(new Shift(true));
-    y.toggleWhenPressed(new Shift(false));
-
-
+    
   }
-
-  public static double getXboxYSpeed() {
-    // if the y axis is more pushed more than a certain amount then (to account for
-    // drivers accidentally
-    // pressing on buttons) then the raw axis will be returned. That value will then
-    // be used to drive
-    // the robot. otherwise the it will return 0 and it will not move. 1 is the Y
-    // Axis on the xbox controller
-    if (Math.abs(xbox.getRawAxis(1)) >= .2) {
-      SmartDashboard.putNumber("Yaxis", xbox.getRawAxis(1));
-      return (xbox.getRawAxis(1));
-    }
-    return 0.0;
-  }
-
-  public static double getXboxXSpeed() {
-    // if the x axis is more pushed more than a certain amount then (to account for
-    // drivers accidentally
-    // pressing on buttons) then the raw axis will be returned. That value will then
-    // be used to drive
-    // the robot. otherwise the it will return 0 and it will not move. 0 is the x
-    // Axis on the xbox controller
-    if (Math.abs(xbox.getRawAxis(5)) >= .2)
+	/**
+	 * Getter for the x-axis of the left joystick
+	 * @return The x-axis on the left joystick which is used for the speed of the right motors on tank drive
+	 */
+  public static double getLeftSpeed() 
+  {
+    if (Math.abs(mainDriver.getRawAxis(1)) >= .2) 
     {
-        return (xbox.getRawAxis(5));
-    }
-    return 0.0;
-  }
-
+      return (mainDriver.getRawAxis(1));
+		}
+		return 0.0;
+	}
+	/**
+	 * Getter for the x-axis of the right joystick
+	 * @return The x-axis on the right joystick which is used for the speed of the right motors on tank drive
+	 */
+	public static double getRightSpeed() 
+	{
+		if(Math.abs(mainDriver.getRawAxis(5)) >= .2)
+		{
+			return (mainDriver.getRawAxis(5));
+		}
+		return 0.0;
+	}
+	
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
+  public Command getAutonomousCommand()
+  {
     return m_autoCommand;
   }
 
-  public Command getDrive()
-  {
-    return driving;
-  }
 }
