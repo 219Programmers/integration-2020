63da2c30bbb6af87d625cb08b7ee85cc78a6cbcc
219Programmers <60157129+219Programmers@users.noreply.github.com>
2020-02-03 15:34:40 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/commands/ModeSwitch.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class ModeSwitch extends CommandBase {
+  /**
+   * Creates a new ModeSwitch.
+   */
+  public ModeSwitch() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.limeSub);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() 
+  {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute()
+  {
+    RobotContainer.limeSub.switchCamera();
+    if(RobotContainer.limeSub.getCamMode() == 0)
+    {
+      RobotContainer.limeSub.setLEDMode(3);
+    }
+    else
+    {
+      RobotContainer.limeSub.setLEDMode(1);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
